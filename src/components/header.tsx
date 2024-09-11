d4629279219315dd44e4393ab72a81b6d18748f1
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-11 18:42:45 -0300

    tentando ajeitar header de novo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,11 @@ import { ShoppingCartIcon, SearchIcon, MapPin } from "lucide-react"
 
 export default function Header() {
     return (
-        <header>
-            <div className="text-white w-screen">
-                <div className="flex flex-wrap bg-black px-2 py-1 items-center gap-1">
-                    <MenuSheet></MenuSheet>
-                    <div className="flex mx-2 py-2 mr-auto">
+        <header className="flex flex-col w-screen text-white">
+            <div className="bg-black flex flex-col p-2 gap-1 pb-4">
+                <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center">
+                        <MenuSheet></MenuSheet>
                         <a href="/">
                             <img
                                 src="favicon.ico"
@@ -18,36 +18,36 @@ export default function Header() {
                             />
                         </a>
                     </div>
-                    <div className="p-2 rounded-sm relative flex">
-                        <ShoppingCartIcon size={28}></ShoppingCartIcon>
-                        <span className=" text-white absolute top-0 left-6 text-center w-6 h-6 bg-slate-600 rounded-full">
-                            0
-                        </span>
-                    </div>
-                    <div className="flex pr-2">
+
+                    <div className="flex justify-between gap-6 items-center p-2">
+                        <div className="rounded-sm relative">
+                            <ShoppingCartIcon className="h-7 w-7"></ShoppingCartIcon>
+                            <span className=" text-white absolute -top-2 left-6 text-center w-6 h-6 bg-slate-600 rounded-full">
+                                0
+                            </span>
+                        </div>
                         <HeaderUserButton></HeaderUserButton>
                     </div>
-                    <div className="w-full flex  mr-0 pt-2 pb-2 ">
-                        <form action="" className="flex flex-grow">
-                            <input
-                                type="text"
-                                className="p-2 flex-grow rounded-l text-black"
-                            />
-                            <button className="px-4 bg-blue-500 hover:bg-blue-300 rounded-tr rounded-br">
-                                <SearchIcon></SearchIcon>
-                            </button>
-                        </form>
-                    </div>
                 </div>
-                <div className="flex bg-slate-600 text-sm px-2 py-1 items-center">
-                    <div className="flex items-center rounded px-2 py-2 ">
-                        <div className="mr-1">
-                            <MapPin></MapPin>
-                        </div>
-                        <div className="flex  leading-tight text-xs">
-                            <p className="mx-1">Olá</p>
-                            <p className="font-bold">Selecione o endereço</p>
-                        </div>
+
+                <form action="" className="flex-grow flex p-1">
+                    <input
+                        type="text"
+                        className="flex-grow rounded-l text-black p-2"
+                    />
+                    <button className="bg-blue-500 hover:bg-blue-300 rounded-tr rounded-br p-2">
+                        <SearchIcon></SearchIcon>
+                    </button>
+                </form>
+            </div>
+            <div className="flex bg-slate-600 text-sm items-center p-2">
+                <div className="flex items-center rounded gap-1">
+                    <MapPin className="h-6 w-6"></MapPin>
+                    <div className="flex leading-tight text-xs">
+                        <span>Olá</span>
+                        <span className="font-bold pl-1">
+                            Selecione o endereço
+                        </span>
                     </div>
                 </div>
             </div>
