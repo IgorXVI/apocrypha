fcfd0da6f71aa3e61f8cdf3bc88c8b63352b5609
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-11 17:55:14 -0300

    substituind svgs por lucide

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,67 +1,7 @@
 import HeaderUserButton from "./header-user-button"
 import MenuSheet from "./menu-sheet"
 
-function SearchIconSVG() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-        >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-        </svg>
-    )
-}
-
-function ShoppingCartSVG() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-        >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-            />
-        </svg>
-    )
-}
-
-function MapPinSVG() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-        >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-            />
-        </svg>
-    )
-}
+import { ShoppingCartIcon, SearchIcon, MapPin } from "lucide-react"
 
 export default function Header() {
     return (
@@ -79,8 +19,8 @@ export default function Header() {
                         </a>
                     </div>
                     <div className="p-2 rounded-sm relative flex">
-                        <ShoppingCartSVG></ShoppingCartSVG>
-                        <span className="bold text-white text-base absolute top-0 left-0 -mt-1 ml-4 text-center w-6 h-6 bg-slate-600 rounded-full">
+                        <ShoppingCartIcon size={28}></ShoppingCartIcon>
+                        <span className=" text-white absolute top-0 left-6 text-center w-6 h-6 bg-slate-600 rounded-full">
                             0
                         </span>
                     </div>
@@ -93,8 +33,8 @@ export default function Header() {
                                 type="text"
                                 className="p-2 flex-grow rounded-l text-black"
                             />
-                            <button className="px-4 bg-blue-500 hover:bg-white rounded-tr rounded-br">
-                                <SearchIconSVG></SearchIconSVG>
+                            <button className="px-4 bg-blue-500 hover:bg-blue-300 rounded-tr rounded-br">
+                                <SearchIcon></SearchIcon>
                             </button>
                         </form>
                     </div>
@@ -102,7 +42,7 @@ export default function Header() {
                 <div className="flex bg-slate-600 text-sm px-2 py-1 items-center">
                     <div className="flex items-center rounded px-2 py-2 ">
                         <div className="mr-1">
-                            <MapPinSVG></MapPinSVG>
+                            <MapPin></MapPin>
                         </div>
                         <div className="flex  leading-tight text-xs">
                             <p className="mx-1">Olá</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,76 +1,77 @@
 import { type Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme"
+import tailwindcssAnimate from "tailwindcss-animate"
 
 export default {
     darkMode: ["class"],
     content: ["./src/**/*.tsx"],
     theme: {
-    	extend: {
-    		fontFamily: {
-    			sans: ["var(--font-geist-sans)", ...fontFamily.sans]
-    		},
-    		colors: {
-    			'darkblue-500': '#485769',
-    			'darkblue-600': '#37475A',
-    			'darkblue-700': '#232F3E',
-    			'darkblue-900': '#191E26',
-    			'darkblue-950': '#131A22',
-    			'gold-100': '#FEBD69',
-    			'gold-200': '#FAA63A',
-    			'orange-650': '#E47911',
-    			'yellow-350': '#F0C14B',
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			}
-    		},
-    		spacing: {
-    			'7': '1.75rem',
-    			'75px': '75px',
-    			'210px': '210px'
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		}
-    	}
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+            },
+            colors: {
+                "darkblue-500": "#485769",
+                "darkblue-600": "#37475A",
+                "darkblue-700": "#232F3E",
+                "darkblue-900": "#191E26",
+                "darkblue-950": "#131A22",
+                "gold-100": "#FEBD69",
+                "gold-200": "#FAA63A",
+                "orange-650": "#E47911",
+                "yellow-350": "#F0C14B",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
+                },
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))",
+                },
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))",
+                },
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                chart: {
+                    "1": "hsl(var(--chart-1))",
+                    "2": "hsl(var(--chart-2))",
+                    "3": "hsl(var(--chart-3))",
+                    "4": "hsl(var(--chart-4))",
+                    "5": "hsl(var(--chart-5))",
+                },
+            },
+            spacing: {
+                "7": "1.75rem",
+                "75px": "75px",
+                "210px": "210px",
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+        },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [tailwindcssAnimate],
 } satisfies Config
