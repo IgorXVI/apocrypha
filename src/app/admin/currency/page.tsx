da47abb54eb397ab48d872fd2eabbb9fd90c9f71
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-13 22:30:32 -0300

    adicionando form para cadastrar moedas

---
+++ src/app/admin/currency/page.tsx
@@ -0,0 +1,100 @@
+"use client"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "~/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "~/components/ui/form"
+import { Input } from "~/components/ui/input"
+
+const formSchema = z.object({
+    label: z
+        .string()
+        .min(1, {
+            message: "Prefixo deve ter ao menos 1 caracter.",
+        })
+        .max(5, {
+            message: "Prefixo deve ter no máximo 5 caracteres.",
+        }),
+    iso4217Code: z
+        .string()
+        .min(3, {
+            message: "Código ISO 4217 deve ter ao menos 3 caracter.",
+        })
+        .max(4, {
+            message: "Código ISO 4217 deve ter no máximo 4 caracteres.",
+        }),
+})
+
+export default function CurrencyForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            label: "",
+            iso4217Code: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="p-5 flex flex-col gap-3"
+            >
+                <FormField
+                    control={form.control}
+                    name="label"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Prefixo</FormLabel>
+                            <FormControl>
+                                <Input placeholder="$" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Esse é o préfixo que vai aparecer antes do valor
+                                monetário.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="iso4217Code"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Código ISO 4217</FormLabel>
+                            <FormControl>
+                                <Input placeholder="EUR" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Esse é o código
+                                <a
+                                    href="https://pt.wikipedia.org/wiki/ISO_4217"
+                                    className="underline mr-1 ml-1"
+                                >
+                                    ISO 4217
+                                </a>
+                                da moeda.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Salvar</Button>
+            </form>
+        </Form>
+    )
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,32 @@ export default function Admin() {
     return (
         <main className="flex gap-5 flex-col items-center">
             <h1>OI ADEMIR</h1>
+            <nav>
+                <li>
+                    <a href="/admin/books">Livros</a>
+                </li>
+                <li>
+                    <a href="/admin/authors">Autores</a>
+                </li>
+                <li>
+                    <a href="/admin/translators">Tradutores</a>
+                </li>
+                <li>
+                    <a href="/admin/publishers">Editoras</a>
+                </li>
+                <li>
+                    <a href="/admin/series">Séries</a>
+                </li>
+                <li>
+                    <a href="/admin/categories">Categorias</a>
+                </li>
+                <li>
+                    <a href="/admin/languages">Línguas</a>
+                </li>
+                <li>
+                    <a href="/admin/currency">Moedas</a>
+                </li>
+            </nav>
         </main>
     )
 }
