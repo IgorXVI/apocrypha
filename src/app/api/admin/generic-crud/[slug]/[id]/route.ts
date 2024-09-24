fdc077160f230d50f7735b13b7157faa8eb37846
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-09-24 12:57:00 -0300

    terminando rotas de crud generico

--- src/app/api/admin/generic-crud/[slug]/[id]/route.ts
+++ src/app/api/admin/generic-crud/[slug]/[id]/route.ts
@@ -16,3 +16,85 @@ export async function GET(_: Request, { params: { slug, id } }: { params: { slug
 
     return Response.json(result.data)
 }
+
+export async function DELETE(_: Request, { params: { slug, id } }: { params: { slug: string; id: string } }) {
+    const result = await decideQueries(slug).deleteOne(id)
+
+    if (!result.success) {
+        return Response.json(
+            {
+                errorMessage: result.errorMessage,
+            },
+            {
+                status: 400,
+            },
+        )
+    }
+
+    return Response.json({
+        success: true,
+    })
+}
+
+export async function PATCH(req: Request, { params: { slug, id } }: { params: { slug: string; id: string } }) {
+    const slugger = decideQueries(slug)
+
+    const reqBodyResult = await req
+        .json()
+        .then((data) => {
+            return {
+                success: true,
+                data,
+                errorMessage: "",
+            }
+        })
+        .catch((error) => {
+            return {
+                success: false,
+                data: undefined,
+                errorMessage: (error as Error).message,
+            }
+        })
+
+    if (!reqBodyResult.success) {
+        return Response.json(
+            {
+                errorMessage: reqBodyResult.errorMessage,
+            },
+            {
+                status: 400,
+            },
+        )
+    }
+
+    const validationResult = slugger.validationSchema.safeParse(reqBodyResult.data)
+
+    if (!validationResult.success) {
+        return Response.json(
+            {
+                issues: validationResult.error.issues,
+            },
+            {
+                status: 400,
+            },
+        )
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const result = await slugger.updateOne(id, validationResult.data as any)
+
+    if (!result.success) {
+        return Response.json(
+            {
+                errorMessage: result.errorMessage,
+            },
+            {
+                status: 400,
+            },
+        )
+    }
+
+    return Response.json({
+        success: true,
+    })
+}
