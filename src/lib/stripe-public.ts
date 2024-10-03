4e10720cb677a4e49adc743700ab5ae53bbd1b04
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-03 15:55:59 -0300

    removendo stripe-public

--- src/lib/stripe-public.ts
+++
@@ -1,9 +0,0 @@
-import { type Stripe, loadStripe } from "@stripe/stripe-js"
-
-let stripePromise: Promise<Stripe | null> | undefined
-export const getStripe = () => {
-    if (!stripePromise) {
-        stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
-    }
-    return stripePromise
-}
