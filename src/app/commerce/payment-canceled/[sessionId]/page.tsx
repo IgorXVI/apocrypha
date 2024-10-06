83f480e9ef6665728a92c5fc886d398bb5ddbced
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-06 09:21:42 -0300

    arrumando outro bug no payment canceled

--- src/app/commerce/payment-canceled/[sessionId]/page.tsx
+++ src/app/commerce/payment-canceled/[sessionId]/page.tsx
@@ -1,8 +1,36 @@
+import { auth } from "@clerk/nextjs/server"
 import Link from "next/link"
 import { db } from "~/server/db"
 import { stripe } from "~/server/stripe-api"
 
 export default async function PaymentCanceled({ params: { sessionId } }: { params: { sessionId: string } }) {
+    const user = auth()
+
+    if (!user.userId) {
+        return <div>Não autorizado</div>
+    }
+
+    const existingOrder = await db.order.findFirst({
+        where: {
+            sessionId,
+            userId: user.userId,
+        },
+    })
+
+    if (existingOrder) {
+        return (
+            <div className="flex flex-col items-center justify-center">
+                <p>Pedido ja foi criado.</p>
+                <Link
+                    href={`/commerce/user-order/${existingOrder.id}`}
+                    className="text-blue-500 text-center"
+                >
+                    Ver pedido
+                </Link>
+            </div>
+        )
+    }
+
     const session = await stripe.checkout.sessions.retrieve(sessionId)
 
     if (session.status === "expired") {
