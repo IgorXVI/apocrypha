1d4eb5569560d2ecd02c8e188fa3f777593c9fb1
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-07 13:32:02 -0300

    tentando melhorar o logo e falhando miseravelmente

--- src/app/commerce/_components/hero-section.tsx
+++ src/app/commerce/_components/hero-section.tsx
@@ -1,31 +1,21 @@
-import { BookOpenTextIcon } from "lucide-react"
 import Link from "next/link"
 import { Button } from "~/components/ui/button"
-import { env } from "~/env"
 
 export default async function HeroSection() {
     return (
-        <section className="bg-gradient-to-b from-black to-white min-h-[600px]">
-            <div className="grid py-8 lg:gap-8 lg:py-16 lg:grid-cols-12">
-                <div className="mt-5 px-8 mx-auto place-self-center lg:col-span-8 md:block grid place-items-center">
-                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white flex flex-row gap-3 items-center">
-                        <BookOpenTextIcon className="w-16 h-16 md:w-20 md:h-20" />
-                        {env.APP_NAME}
-                    </h1>
-                    <p className="max-w-xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-neutral-200">
-                        Descubra seu próximo livro favorito, explore nossa vasta coleção de livros em todos os gêneros.
-                    </p>
-                    <Button size="lg">
-                        <Link
-                            href="/commerce/book"
-                            className="text-lg"
-                        >
-                            Começar a explorar
-                        </Link>
-                    </Button>
-                </div>
-
-                <div className="hidden lg:mt-0 lg:col-span-4 lg:flex"></div>
+        <section className="bg-gradient-to-b from-black to-white min-h-[400px]">
+            <div className="py-8 lg:py-16 grid place-items-center gap-10">
+                <p className="max-w-4xl text-3xl text-neutral-200">
+                    Descubra seu próximo livro favorito, explore nossa vasta coleção de livros em todos os gêneros.
+                </p>
+                <Button size="lg">
+                    <Link
+                        href="/commerce/book"
+                        className="text-lg"
+                    >
+                        Explorar
+                    </Link>
+                </Button>
             </div>
         </section>
     )
--- src/components/header/company-icon-button.tsx
+++ src/components/header/company-icon-button.tsx
@@ -1,4 +1,4 @@
-import { BookOpenTextIcon } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 import { env } from "~/env"
 
@@ -8,7 +8,12 @@ export default function CompanyIconButton() {
             href="/commerce"
             className="flex items-center space-x-2"
         >
-            <BookOpenTextIcon className="h-6 w-6" />
+            <Image
+                src="images/favicon-dark.svg"
+                alt="Logo"
+                width={50}
+                height={50}
+            />
             <span className="text-xl font-bold">{env.APP_NAME}</span>
         </Link>
     )
