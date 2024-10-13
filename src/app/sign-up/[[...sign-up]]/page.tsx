6431fb6ce07153868d6ed6aaa888cb2c5d7e1076
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-13 16:25:49 -0300

    mudando background em sign in e sign uo

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,8 @@ import { SignUp } from "@clerk/nextjs"
 
 export default function Page() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen bg-tentacles-mobile md:bg-tentacles bg-cover bg-black">
+            <h1>Apocrypha</h1>
             <SignUp forceRedirectUrl="/commerce" />
         </div>
     )
