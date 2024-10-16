f5716a57236b3014e3b8d2455b133a74eab1926d
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-16 08:56:59 -0300

    salvando dados de favoritos e carrinho de compras no local storage

--- src/components/redux/StoreProvider.tsx
+++ src/components/redux/StoreProvider.tsx
@@ -2,13 +2,43 @@
 
 import { useRef } from "react"
 import { Provider } from "react-redux"
+import { type BookCartState } from "~/lib/redux/book-cart/bookCartSlice"
 import { makeStore, type AppStore } from "~/lib/redux/store"
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
     const storeRef = useRef<AppStore>()
     if (!storeRef.current) {
         // Create the store instance the first time this renders
-        storeRef.current = makeStore()
+
+        const isInBrowser = typeof window !== "undefined"
+
+        let bookFavsPreloaded: BookCartState[] = []
+        let bookCartPreloaded: BookCartState[] = []
+
+        if (isInBrowser) {
+            bookFavsPreloaded = JSON.parse(window.localStorage.getItem("bookFavs") ?? "[]")
+            bookCartPreloaded = JSON.parse(window.localStorage.getItem("bookCart") ?? "[]")
+        }
+
+        storeRef.current = makeStore({
+            bookFavs: {
+                value: bookFavsPreloaded,
+            },
+            bookCart: {
+                value: bookCartPreloaded,
+            },
+        })
+
+        storeRef.current.subscribe(() => {
+            const state = storeRef.current?.getState()
+
+            if (!state || !isInBrowser) {
+                return
+            }
+
+            window.localStorage.setItem("bookFavs", JSON.stringify(state.bookFavs.value))
+            window.localStorage.setItem("bookCart", JSON.stringify(state.bookCart.value))
+        })
     }
 
     return <Provider store={storeRef.current}>{children}</Provider>
--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { bookCartSlice } from "./book-cart/bookCartSlice"
+import { bookCartSlice, type BookCartState } from "./book-cart/bookCartSlice"
 import { mainApi } from "./apis/main-api/main"
 import { brasilApi } from "./apis/brasil-api/brasil"
 import { bookFavsSlice } from "./book-favs/bookFavsSlice"
 
-export const makeStore = () => {
-    return configureStore({
+export const makeStore = (preloadedState: {
+    bookFavs: {
+        value: BookCartState[]
+    }
+    bookCart: {
+        value: BookCartState[]
+    }
+}) =>
+    configureStore({
         reducer: {
             bookCart: bookCartSlice.reducer,
             bookFavs: bookFavsSlice.reducer,
@@ -13,8 +20,8 @@ export const makeStore = () => {
             [brasilApi.reducerPath]: brasilApi.reducer,
         },
         middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(mainApi.middleware).concat(brasilApi.middleware),
+        preloadedState,
     })
-}
 
 export type AppStore = ReturnType<typeof makeStore>
 export type RootState = ReturnType<AppStore["getState"]>
