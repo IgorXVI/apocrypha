5efae4ed81115083adc5363ce3bccbc192e025c0
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-20 23:32:33 -0300

    seed para salvar imagens no s3

--- node-scripts/livraria-cultura/pre-seed-db.js
+++ node-scripts/livraria-cultura/pre-seed-db.js
@@ -7,12 +7,6 @@ import fs from "fs"
 import path from "path"
 import { z } from "zod"
 
-import Stripe from "stripe"
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-    apiVersion: "2024-06-20",
-})
-
 neonConfig.webSocketConstructor = ws
 const connectionString = `${process.env.DATABASE_URL}`
 
