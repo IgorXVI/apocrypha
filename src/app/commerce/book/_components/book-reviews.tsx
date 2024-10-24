2d603af5322ec82135fd5ac21a4675ab869641cc
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-10-24 17:03:51 -0300

    adicionando imagens de usuario para as reviews

--- src/app/commerce/book/_components/book-reviews.tsx
+++ src/app/commerce/book/_components/book-reviews.tsx
@@ -1,10 +1,12 @@
-import { Avatar, AvatarFallback } from "~/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
 import { Card, CardContent } from "~/components/ui/card"
 import { Separator } from "~/components/ui/separator"
 import { calcSkip } from "~/lib/utils"
 import { db } from "~/server/db"
 import ReviewStars from "./review-stats"
 import PaginationNumbers from "~/components/pagination/pagination-numbers"
+import { authClient } from "~/server/auth-api"
+import { type User } from "@clerk/nextjs/server"
 
 export default async function BookReviews({
     bookId,
@@ -17,8 +19,9 @@ export default async function BookReviews({
     reviewsCount: number
     reviewsPage?: string
 }) {
-    const currentReviewsPage = Number(reviewsPage) || 1
-    const currentReviewsTake = 5
+    const page = Number(reviewsPage) || 1
+    const take = 5
+    const skip = calcSkip(page, take)
 
     const bookReviews = await db.review.findMany({
         where: {
@@ -27,9 +30,31 @@ export default async function BookReviews({
         orderBy: {
             createdAt: "desc",
         },
-        take: currentReviewsTake,
-        skip: calcSkip(currentReviewsPage, currentReviewsTake),
+        take,
+        skip,
     })
+
+    const userDataMap: Record<string, User> = {}
+    const userDefaultName: Record<string, string> = {}
+
+    if (bookReviews.length > 0) {
+        const userData = await authClient.users.getUserList({
+            limit: take,
+            userId: bookReviews.map((review) => review.userId),
+        })
+
+        userData.data.forEach((user) => {
+            userDataMap[user.id] = user
+        })
+
+        bookReviews.forEach((review) => {
+            userDefaultName[review.userId] = review.userName
+                .split(" ")
+                .filter((_, i, arr) => i === 0 || i === arr.length - 1)
+                .map((s) => s.charAt(0))
+                .join("")
+        })
+    }
 
     return bookReviews.length > 0 ? (
         <>
@@ -48,13 +73,8 @@ export default async function BookReviews({
                     <CardContent className="p-4 flex flex-col gap-1">
                         <div className="flex items-center justify-start gap-3 mb-3">
                             <Avatar className="h-10 w-10">
-                                <AvatarFallback>
-                                    {review.userName
-                                        .split(" ")
-                                        .filter((_, i, arr) => i === 0 || i === arr.length - 1)
-                                        .map((s) => s.charAt(0))
-                                        .join("")}
-                                </AvatarFallback>
+                                <AvatarImage src={userDataMap[review.userId]?.imageUrl}></AvatarImage>
+                                <AvatarFallback>{userDefaultName[review.userId]}</AvatarFallback>
                             </Avatar>
                             <div className="ml-4">
                                 <div className="flex flex-row gap-2">
@@ -74,8 +94,8 @@ export default async function BookReviews({
                 </Card>
             ))}
             <PaginationNumbers
-                page={currentReviewsPage}
-                take={currentReviewsTake}
+                page={page}
+                take={take}
                 total={reviewsCount}
                 urlPageParamName={reviewsPageName}
                 sectionId="book-reviews"
