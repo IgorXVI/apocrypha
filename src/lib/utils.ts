a8beea5e053eb1ff38926dd3fd605a49a220c678
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2024-11-06 10:55:22 -0300

    adicionando estimativa de entrega aos pedidos que estao a caminho

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,20 +12,5 @@ export const calcSkip = (page: number, take: number) => take * (page - 1)
 const getDateOffset = (date: Date, offsetInDays: number) =>
     new Date(date.getTime() + offsetInDays * 24 * 60 * 60 * 1000).toLocaleDateString("pt-BR", { timeZone: "America/Sao_Paulo" })
 
-const calcShippingDate = (updatedAt: Date, min: number, max: number) =>
+export const calcShippingDate = (updatedAt: Date, min: number, max: number) =>
     min === max ? getDateOffset(updatedAt, min) : `Entre ${getDateOffset(updatedAt, min)} e ${getDateOffset(updatedAt, max)}`
-
-export const calcDeliveryLocalDate = ({
-    updatedAt,
-    status,
-    shippingDaysMin,
-    shippingDaysMax,
-}: {
-    updatedAt: Date
-    status: string
-    shippingDaysMin: number
-    shippingDaysMax: number
-}) =>
-    status === "DELIVERED"
-        ? updatedAt.toLocaleDateString("pt-BR", { timeZone: "America/Sao_Paulo" })
-        : calcShippingDate(updatedAt, shippingDaysMin, shippingDaysMax)
